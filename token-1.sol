9d0489887abf591b4b7dabce7953ebb8118d1c65
Anurag Nayak <nayakanurag005@gmail.com>
2024-06-25 12:23:48 +0530

    Update token-1.sol

--- token-1.sol
+++ token-1.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.18;
+pragma solidity ^0.8.18;
 
 /*
        REQUIREMENTS
@@ -7,38 +7,34 @@ pragma solidity 0.8.18;
     2. Your contract will have a mapping of addresses to balances (address => uint)
     3. You will have a mint function that takes two parameters: an address and a value. 
        The function then increases the total supply by that number and increases the balance 
-       of the address by that amount
+       of the address by that amount.
     4. Your contract will have a burn function, which works the opposite of the mint function, as it will destroy tokens. 
-       It will take an address and value just like the mint functions. It will then deduct the value from the total supply 
-       and from the balance of the addresss.
-    5. Lastly, your burn function should have conditionals to make sure the balance of account is greater than or equal 
+       It will take an address and value just like the mint function. It will then deduct the value from the total supply 
+       and from the balance of the address.
+    5. Lastly, your burn function should have conditionals to make sure the balance of the account is greater than or equal 
        to the amount that is supposed to be burned.
 */
 
-contract MyToken 
-{
+contract MyToken {
 
     // Public variables here
     string public tokenName = "META";
     string public tokenAbbrv = "MTA";
-    uint public totalSupply = 0;
+    uint256 public totalSupply;
 
     // Mapping variable here
-    mapping(address => uint) public balances;
+    mapping(address => uint256) public balances;
 
     // Mint function
-    function mint(address _address, uint _value) public {
+    function mint(address _address, uint256 _value) public {
         totalSupply += _value;
         balances[_address] += _value;
     }
 
     // Burn function
-    function burn(address _address, uint _value) public 
-    {
-        require(balances[_address] >= _value); 
-        {
+    function burn(address _address, uint256 _value) public {
+        require(balances[_address] >= _value, "Insufficient balance to burn");
         totalSupply -= _value;
         balances[_address] -= _value;
-        }
     }
 }
